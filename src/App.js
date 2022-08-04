ae5cde6531dad8ee3674def11e13624c0fd67a3a
Alexandre benjamin <alexandre.benjamin@gmail.com>
2022-08-04 19:42:32 -0300

    nova branch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import CreditCard from './components/CreditCard';
+import CardList from './components/CardList';
+import CentereButton from './components/CentereButton';
 import "./App.scss";
+
+import posts from './data/posts';
 
 const App = () => (
     <div className="App">
       <Navbar />
       <Hero />
-    </div>
-  );
+    
+  <CreditCard />
+  <CardList post={posts}/>
+  <CentereButton> Abra sua conta <CentereButton />
 
+</div>
 
+);
 export default App;
---
+++ src/assets/posts.js
@@ -0,0 +1,31 @@
+import image1 from '../assets/post-1.jpg';
+import image2 from '../assets/post-2.jpg';
+import image3 from '../assets/post-3.jpg';
+
+
+const posts = [
+    {
+        id: 1,
+        image: image1,
+        title: 'Lorem ipson dolor',
+        excerpt: 'Lorem ipson dolor sit, Lorem ipson dolor sit,Lorem ipson dolor sit',
+        action: 'Peça já',
+    },
+    {
+        id: 2,
+        image: image2,
+        title: 'Lorem ipson dolor sit',
+        excerpt: 'Lorem ipson dolor sit, Lorem ipson dolor sit,Lorem ipson dolor sit',
+        action: 'Peça já',
+    },
+    {
+        id: 3,
+        image: image3,
+        title: 'Lorem ipson sit',
+        excerpt: 'Lorem ipson dolor sit, Lorem ipson dolor sit,Lorem ipson dolor sit',
+        action: 'Peça já',
+    },
+];
+
+export default posts;
+
